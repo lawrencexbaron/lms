2fcd54495c15ae6c101f17ee57126d75cc57b762
Lawrence <lawrencebaron027@gmail.com>
2023-11-15 21:11:39 -0700

    Add grades, sections, and students tables to database migrations

--- database/migrations/2023_11_12_205832_create_sections_table.php
+++ database/migrations/2023_11_12_205832_create_sections_table.php
@@ -16,8 +16,11 @@ return new class extends Migration
             $table->string('name');
             $table->integer('capacity')->default(0);
             $table->foreignId('adviser_id')->constrained('users')->onDelete('cascade'); // 'users' table is the default table name for the model 'User
+            $table->foreignId('grade_level_id')->nullabe()->constrained('grades')->onDelete('cascade');
+            $table->foreignId('school_year_id')->nullabe()->constrained('school_years')->onDelete('cascade');
             $table->string('section_code');
             $table->string('section_description')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
