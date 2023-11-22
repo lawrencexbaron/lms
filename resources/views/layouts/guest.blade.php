a929ed7de6d7987c9546c269615991aacd930548
Lawrence <lawrencebaron027@gmail.com>
2023-11-21 22:15:26 -0700

    Update guest layout styles

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,15 +14,15 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
+    <body class="font-sans bg-gray-50 text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:flex-row sm:justify-between items-center pt-6 sm:pt-0 bg-gray-100 w-full">
             {{-- <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div> --}}
-            <div class="w-1/2 bg-blue-300 min-h-screen">
+            <div class="w-1/2  min-h-screen">
 
             </div>
-            <div class="w-1/2 min-h-screen flex items-center justify-center">
+            <div class="w-1/2 shadow-lg border-left min-h-screen flex items-center justify-center">
                 <div class="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white border overflow-hidden sm:rounded-lg">
                     {{ $slot }}
                 </div>
